435afa68605e33e02e0c706c70ccb273698057c7
wy <wy@localhost.localdomain>
2017-12-20 21:01:58 +0800

    初始化项目

---
+++ src/main/java/com/wy/centosafe/CentosafeApplication.java
@@ -0,0 +1,12 @@
+package com.wy.centosafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentosafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentosafeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wy/centosafe/CentosafeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wy.centosafe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CentosafeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
